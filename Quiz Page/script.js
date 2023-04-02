6be821c60ed5d6dbab5bdba895489953a52b4162
Laurie Fish <iamlauriefish@gmail.com>
2023-04-01 17:48:11 -0700

    moved question/answers over to js

---
+++ Quiz Page/script.js
@@ -0,0 +1,51 @@
+const quiz = document.getElementById("quiz");
+const question =document.getElementById("question");
+
+
+
+
+
+
+const allQuestions = [
+    {
+        
+        question: "What does the C in Crud stand for?",
+        answer:  "create",
+        choices: [
+            "Construct",
+            "Copy",
+            "Create", 
+            "cut"
+        ]},
+        {
+           
+            question: "what does API stand for?",
+            answer: "Application Program Interface",
+            choices: [
+                "Application Program Interface",
+                "Application Private Interface",
+                "Application Program Index",
+                "Absolute Program Interface"
+            ]},
+            {
+                
+                question: "What is a taco",
+                answer: "XXX",
+                options: [
+                    "YYYY",
+                    "BBB",
+                    "CCC",
+                    "xxx"
+                ]}, 
+            {
+               
+                question: "What is a burrito",
+                answer: "AAA",
+                options: [
+                    "MMM",
+                    "KKK",
+                    "AAA",
+                    "FFF"
+                ]   
+    }
+]
