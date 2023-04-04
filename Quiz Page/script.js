6feb120ca0f769e6a412833465ed3745faa97074
Laurie Fish <iamlauriefish@gmail.com>
2023-04-03 18:57:40 -0700

    added final score and input score functions

--- Quiz Page/script.js
+++ Quiz Page/script.js
@@ -6,12 +6,22 @@ const answer2 = document.getElementById("answer-2-text");
 const answer3 = document.getElementById("answer-3-text");
 const answer4 = document.getElementById("answer-4-text");
 const enterButton = document.getElementById("enter");
-const startButton = document.getElementById("start-button")
+const startButton = document.getElementById("start-button");
+const scoreButton = document.getElementById("view-high-scores");
+var inputInitials = document.getElementById("input-initials");
+var initialsButton = document.getElementById("initials-button");
 var secCount = 60
 var timer = document.getElementById("timer");
 var timerId;
-startButton.addEventListener("click", startQuiz); 
+var totalScore = document.getElementById("final-score");
+const seeQuiz = document.getElementById("container-2");
+var finishedBox = document.getElementById("finished-box-container");
 
+finishedBox.style.visibility = "hidden";
+
+
+startButton.addEventListener("click", startQuiz); 
+// scoreButton.addEventListener("click", )
 enterButton.addEventListener("click", getAnswer);
     
 
@@ -52,23 +62,23 @@ const allQuestions = [
             b: "A parameter",
             c: "An object",
             d: "An element",
-            answer: "d"
+            answer: "c"
     }
 ];
 let quizStart = 0;
 let score = 0;
 let counter;
 
+// function displayScore () {
 
 
-function timer() {
-    secCount--;
-    timer.textContent = secCount;
-}
+
+
+
 function startQuiz () {
-    timerId = setInterval(timer, 1000);
+    // timerId = setInterval(timer, 1000);
     const quizStartData = allQuestions[quizStart];
-    const seeQuiz = document.getElementById("container-2");
+    
     const noIntro = document.getElementById("intro");
     noIntro.style.visibility = "hidden";
     seeQuiz.style.display = "flex";
@@ -77,7 +87,6 @@ function startQuiz () {
     answer2.innerText = quizStartData.b
     answer3.innerText = quizStartData.c
     answer4.innerText = quizStartData.d
-    timer();
 }
 function getAnswer() {
     var answer = "";
@@ -92,13 +101,22 @@ function getAnswer() {
                 quizStart++;
                 alert ("wrong");
             }
+            totalScore.innerHTML = "Your score is " + score;
         } 
     }
-    clearAnswers();
-    startQuiz();
-    
+        clearAnswers();
+        if(quizStart < allQuestions.length) {
+            startQuiz();
+        } else {
+            seeQuiz.style.visibility = "hidden";
+            finishedBox.style.visibility = "visible";
+            inputScore();
+        }
+        
 }
 
+
+    
 
 
 function clearAnswers() {
@@ -108,6 +126,7 @@ function clearAnswers() {
     }
 }
 
+
 /*function startTimer(time){
     counter = setInterval(timer, 1000);
 
@@ -115,5 +134,24 @@ function timer () {
     secCount.textContent = time;
     time--;
     }
+    function timer() {
+    secCount--;
+    timer.textContent = secCount;
     
 }*/
+ function inputScore () {
+    initialsButton.addEventListener("click", displayFinalScore);
+ }
+ function displayFinalScore () {
+    localStorage.setIem(inputInitials.value, score);
+    
+    var listEl = document.getElementById("list");
+    for(var i = 0; i < localStorage.length; i++) {
+        var newListItem = document.createElement("li");
+        localStorage.key(i);
+        listEl.innerHTML = ""+localStorage.key(i) + " " +localStorage.getItem(localStorage.key(i));
+        listEl.appendChild(newListItem);
+        
+
+    }
+ }    
